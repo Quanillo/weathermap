759e5911a3205e996e39e670badda35767335d3b
Juan <j.garrigos91@gmail.com>
2022-12-03 13:48:43 +0100

    Vite moving

--- README.md
+++
@@ -1,7 +0,0 @@
-Work in progress....
-
-Install:
-npm install<br>
-npm install axios<br>
-npm install react react-dom leaflet<br>
-npm install react-leaflet
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import './App.css';
+import './App.css'
 import { MapView } from './components/maps/MapView'
 import { Weather } from './components/Weather'
 import { Air } from './components/Air'
 import { Place } from './components/Place'
 
-const key = process.env.REACT_APP_API_KEY
+const key = import.meta.env.VITE_API_KEY
 
 function App() {
   const [pos, setPos] = useState({ data: null });
@@ -63,50 +63,4 @@ function App() {
   );
 }
 
-export default App;
-
-
-/*
-
-const [ country, setCountry ] = useState([])
-        function putCountry() {
-            axios.get(`https://restcountries.com/v3.1/alpha/${place.country}`)
-                .then((data) => {
-                    setCountry(data.data);
-                });
-        }
-
-
- setInfo={setInfo}
-
-          function handleWeather() {
-    console.log(pos)
-      axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${pos.lat}&lon=${pos.lng}&units=metric&appid=${key}`)
-          .then((data) => {
-          setWeather(data.data);
-          });
-  }
-
-  function handleAir() {
-    axios.get(`http://api.openweathermap.org/data/2.5/air_pollution?lat=${pos.lat}&lon=${pos.lng}&appid=${key}`)
-        .then((data) => {
-        setAir(data.data);
-        });
-  }   
-
-  function handlePlace() {
-    axios.get(`http://api.openweathermap.org/geo/1.0/reverse?lat=${pos.lat}&lon=${pos.lng}&appid=${key}`)
-        .then((data) => {
-          setPlace(data.data[0]);
-        });
-  }   
-
-
-  
-  function setInfo(){
-    handleWeather();
-    handleAir();
-    handlePlace();
-    console.log(pos)
-  }
-*/
+export default App
